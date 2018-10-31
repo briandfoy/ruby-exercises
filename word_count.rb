4329c51b24ee1a59775c1d965d701349fadbbe69
brian d foy <brian.foy@roivant.com>
2018-10-31 10:05:32 -0400

    Reformatted with rubocop

--- word_count.rb
+++ word_count.rb
@@ -1,18 +1,18 @@
-def words_from_string( string )
-	string.downcase.scan( /[\w']+/ )
+def words_from_string(string)
+  string.downcase.scan(/[\w']+/)
 end
 
-def count_frequency( list )
-	counts = Hash.new(0)
-	list.each do |word|
-	  counts[word] += 1
+def count_frequency(list)
+  counts = Hash.new(0)
+  list.each do |word|
+    counts[word] += 1
   end
   counts
 end
 
 count_frequency(
-  words_from_string( 'Ruby! Hello Ruby' )
-).sort_by { |word,count| count }
+  words_from_string('Ruby! Hello Ruby')
+).sort_by { |_, count| count }
   .reverse
   .take(5)
   .each { |t| p t }
