4982d4951188447905dde87fc088648ac4708ecf
brian d foy <brian.foy@roivant.com>
2018-10-31 09:40:24 -0400

    Convert to method chaining

--- word_count.rb
+++ word_count.rb
@@ -10,15 +10,9 @@ def count_frequency( list )
   counts
 end
 
-words = words_from_string( 'Ruby! Hello Ruby' )
-counts = count_frequency( words )
-p words
-p counts
-
-sorted = counts.sort_by { |word,count| count }
-p sorted
-
-sorted.reverse.take(5).each do |tuple|
-  p tuple
-end
-
+count_frequency(
+  words_from_string( 'Ruby! Hello Ruby' )
+).sort_by { |word,count| count }
+  .reverse
+  .take(5)
+  .each { |t| p t }
